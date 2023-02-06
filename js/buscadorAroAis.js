276c38289f3432af349bd7768c18180cae84e0ad
Felipe <amoabilliken@gmail.com>
2023-02-06 10:29:23 -0300

    Agregado de JS y cambios en el front

---
+++ js/buscadorAroAis.js
@@ -0,0 +1,8 @@
+const aroAisOffices = [
+    {aroAisOff: "SABE - AEROPARQUE", aroAisTelNumber: "(011)4514-1513", aroAisEmail: "acaer@eana.com.ar", aroAisInfo: "Oficina H24"},
+    {aroAisOff: "SAZB - BAHIA BLANCA", aroAisTelNumber: "(0291)4860300", aroAisEmail: "acbca@eana.com.ar", aroAisInfo: "Oficina H24"},
+    {aroAisOff: "SAVC - COMODORO RIVADAVIA", aroAisTelNumber: "(02970)4549439", aroAisEmail: "accrv@eana.com.ar", aroAisInfo: "Oficina H24"},
+    {aroAisOff: "SACO - CORDOBA", aroAisTelNumber: "(0351)4751404", aroAisEmail: "accba@eana.com.ar", aroAisInfo: "Oficina H24"},
+    {aroAisOff: "SAEZ - EZEIZA", aroAisTelNumber: "(011)44802330", aroAisEmail: "aceze@eana.com.ar", aroAisInfo: "Oficina H24"}
+]
+
---
+++ js/main.js
@@ -0,0 +1,34 @@
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+})
+
+swalWithBootstrapButtons.fire({
+    title: '¡Hola! Bienvenido a Plan Control.',
+    text: "¿Sabés como utilizar Plan Control?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'No, quiero ver el tutorial',
+    cancelButtonText: '¿Cómo? ¡Yo ya se usar Plan Control!',
+    reverseButtons: true
+}).then((result) => {
+    if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+        )
+    } else if (
+      /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+    ) {
+        swalWithBootstrapButtons.fire(
+        'Cancelled',
+        'Your imaginary file is safe :)',
+        'error'
+        )
+    }
+    })
