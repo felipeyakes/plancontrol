2a44c546ecd138133d016bb8362d68b16300d2e8
Felipe <amoabilliken@gmail.com>
2023-02-07 12:02:50 -0300

    Cambios varios

--- js/buscadorAroAis.js
+++ js/buscadorAroAis.js
@@ -6,3 +6,25 @@ const aroAisOffices = [
     {aroAisOff: "SAEZ - EZEIZA", aroAisTelNumber: "(011)44802330", aroAisEmail: "aceze@eana.com.ar", aroAisInfo: "Oficina H24"}
 ]
 
+const formulario = document.querySelector('#formulario');
+const boton = document.querySelector('#boton');
+const resultado = document.querySelector('#resultado')
+
+const filtrar = ()=>{
+    // console.log(formulario.value);
+    resultado.innerHTML = ``;
+
+    const texto = formulario.value.toLowerCase();
+    for(let aroAisOffice of aroAisOffices){
+        let aroAisOff = aroAisOffice.aroAisOff.toLowerCase();
+        if(aroAisOff.indexOf(texto) !== -1){
+            resultado.innerHTML += `<li>${aroAisOffice.aroAisOff}</li>`
+        }
+    }
+    
+    if(resultado.innerHTML === ``){
+        resultado.innerHTML += `<li>Oficina ARO AIS no encontrada</li>`
+    }
+}
+
+boton.addEventListener('click', filtrar)
